0ac63fbd1f1e692d38fa9c0264c5c666de8a4f96
Greymussolutions <gayisi0901@gmail.com>
2026-07-09 14:18:20 +0300

    Set up Firebase configuration and initialization

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
-// Placeholder
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDt9iW5woMIRP1E-eMTHdYpgceQZxK8wrE",
+  authDomain: "greymus-loan.firebaseapp.com",
+  projectId: "greymus-loan",
+  storageBucket: "greymus-loan.firebasestorage.app",
+  messagingSenderId: "689969179781",
+  appId: "1:689969179781:web:aca0247196d31dcd68cdec"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
