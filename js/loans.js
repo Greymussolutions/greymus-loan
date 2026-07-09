8ba31df0c5a0a8b278f97b29cf2dcd83a52fe68b
Greymussolutions <gayisi0901@gmail.com>
2026-07-09 17:00:49 +0300

    Disable loan functions in loans.js
    Disable addLoan function and remove getLoans implementation.

--- js/loans.js
+++ js/loans.js
@@ -1,23 +1,8 @@
 // loans.js
-
-import { db } from "./firebase.js";
-import {
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const loansCollection = collection(db, "loans");
-
 export async function addLoan(loan) {
-  return await addDoc(loansCollection, loan);
+  console.log("addLoan disabled", loan);
 }
 
 export async function getLoans() {
-  const snapshot = await getDocs(loansCollection);
-
-  return snapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  }));
+  return [];
 }
