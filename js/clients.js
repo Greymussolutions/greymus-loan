c13bfccd1aaf63d11cbb5be083f9a79acdd085a6
Greymussolutions <gayisi0901@gmail.com>
2026-07-09 16:59:52 +0300

    Disable client addition and retrieval functions
    Comment out Firestore functionality for clients.

--- js/clients.js
+++ js/clients.js
@@ -1,22 +1,8 @@
 // clients.js
-
-import { db } from "./firebase.js";
-import {
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const clientsCollection = collection(db, "clients");
-
 export async function addClient(client) {
-  return await addDoc(clientsCollection, client);
+  console.log("addClient disabled", client);
 }
 
 export async function getClients() {
-  const snapshot = await getDocs(clientsCollection);
-  return snapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  }));
+  return [];
 }
